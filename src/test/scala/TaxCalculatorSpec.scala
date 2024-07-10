79c79bf3ccc4e0abde2de8533fa9250168072e91
Antony Angeli <antony.angeli@Antonys-Laptop.local>
2024-07-10 17:20:24 +0100

    add calculateTax tests and method, add isHigherRateTaxpayer tests

--- src/test/scala/TaxCalculatorSpec.scala
+++ src/test/scala/TaxCalculatorSpec.scala
@@ -15,4 +15,47 @@ class TaxCalculatorSpec extends AnyWordSpec {
       }
     }
   }
+
+  "TaxCalculator.calculateTax" should {
+    "return the total amount of tax to pay" when {
+      "the income is above the personal tax limit but below or equal to the basic rate limit" in {
+        val result: Double = taxCalculator.calculateTax(20000)
+
+        assert(result == 4000)
+      }
+    }
+  }
+
+  "TaxCalculator.calculateTax" should {
+    "return the total amount of tax to pay" when {
+      "the income is above the basic tax limit but below or equal to the higher rate limit" in {
+        val result: Double = taxCalculator.calculateTax(60000)
+
+        assert(result == 24000)
+      }
+    }
+  }
+
+
+  /** is higher rate taxpayer */
+  "TaxCalculator.isHigherRateTaxpayer" should {
+    "return true" when {
+      "when the income falls into the higher rate tax band" in {
+        val result: Boolean = taxCalculator.isHigherRateTaxpayer(60000)
+
+        assert(result)
+      }
+    }
+  }
+
+  "TaxCalculator.isHigherRateTaxpayer" should {
+    "return false" when {
+      "when the income is not in higher rate tax band" in {
+        val result: Boolean = taxCalculator.isHigherRateTaxpayer(10000)
+
+        assert(!result)
+      }
+
+    }
+  }
 }
