7a4df82390b986472c096200cd0f345340262dd9
Antony Angeli <antony.angeli@Antonys-Laptop.local>
2024-07-11 15:24:33 +0100

    tax calculator MVP with extension and spec

--- src/main/scala/TaxCalculator.scala
+++ src/main/scala/TaxCalculator.scala
@@ -11,6 +11,11 @@ class TaxCalculator {
   private val higherRate: Double = 0.4
   private val additionalRate: Double = 0.45
 
+  // capital gains shares
+  private val minTaxableAmountForShares = 3000
+  private val capitalGainsRateForBasicRate = 0.1
+  private val capitalGainsRateForHigherRate = 0.2
+
   // A method to calculate the total amount of tax to be paid, returned as a double
   def calculateTax(income: Double): Double = {
     if (income <= personalAllowance) {
@@ -20,20 +25,58 @@ class TaxCalculator {
     } else if (income <= higherRateLimit) {
       income * higherRate
     } else {
-      404.00
+      0.00
     }
 
   }
 
   // A method which can tell you if someone is a higher rate taxpayer
   def isHigherRateTaxpayer(income: Double): Boolean = {
-    ???
+    if (income <= higherRateLimit && income > basicRateLimit) {
+      true
+    } else if (income > higherRateLimit) {
+      true
+    } else {
+      false
+    }
   }
 
   // A method that will return a string with the income limit of their current tax band.
   // The return will also be formatted, E.g: "£12,500" or "No limit"
-  def formattedCurrentTaxAllowance(income: Double): String = {
-    ???
+
+  def formatDoubleToCurrencyString(num:Double): String = {
+    val result:String = f"£$num%,.0f"
+    result
   }
 
+
+  def formattedCurrentTaxAllowance(income: Double): String = {
+    if (income <= personalAllowance) {
+      formatDoubleToCurrencyString(personalAllowance)
+    } else if (income <= basicRateLimit) {
+      formatDoubleToCurrencyString(basicRateLimit)
+    } else if (income <= higherRateLimit) {
+      formatDoubleToCurrencyString(higherRateLimit)
+    } else if (income > higherRateLimit ){
+      "No limit"
+    } else {
+      "error handling income"
+    }
+  }
+
+  def calculateCapitalGainsFromShares(income: Double, sharesValue:Double): String = {
+    if (income <= personalAllowance && sharesValue >= minTaxableAmountForShares) {
+      formatDoubleToCurrencyString(sharesValue * capitalGainsRateForBasicRate)
+    } else if (income <= basicRateLimit && sharesValue >= minTaxableAmountForShares) {
+      formatDoubleToCurrencyString(sharesValue * capitalGainsRateForBasicRate)
+    } else if (income <= higherRateLimit && sharesValue >= minTaxableAmountForShares) {
+      formatDoubleToCurrencyString(sharesValue * capitalGainsRateForHigherRate)
+    } else if (income > higherRateLimit && sharesValue >= minTaxableAmountForShares){
+      formatDoubleToCurrencyString(sharesValue * capitalGainsRateForHigherRate)
+    } else if (sharesValue < minTaxableAmountForShares){
+      "shares are below taxable threshold"
+    } else {
+      "error handling values"
+    }
+  }
 }
--- src/test/scala/TaxCalculatorSpec.scala
+++ src/test/scala/TaxCalculatorSpec.scala
@@ -58,4 +58,121 @@ class TaxCalculatorSpec extends AnyWordSpec {
 
     }
   }
+
+  /** format doubles */
+  "TaxCalculator.formatDoubleToCurrencyString" should {
+    "return a formatted currency string" when {
+      "given a Double" in {
+        val result: String = taxCalculator.formatDoubleToCurrencyString(40000)
+
+        assert(result == "£40,000")
+      }
+    }
+  }
+
+
+/** formatted current tax allowance */
+
+  "TaxCalculator.formattedCurrentTaxAllowance" should {
+    "return a formatted string of the basic tax rate limit" when {
+      "the income falls into the basic tax band bracket" in {
+        val result: String = taxCalculator.formattedCurrentTaxAllowance(40000)
+
+        assert(result == "£50,000")
+      }
+    }
+  }
+
+  "TaxCalculator.formattedCurrentTaxAllowance" should {
+    "return a formatted string of the higher tax rate limit" when {
+      "the income falls into the higher tax band bracket" in {
+        val result: String = taxCalculator.formattedCurrentTaxAllowance(80000)
+
+        assert(result == "£125,000")
+      }
+    }
+  }
+
+  "TaxCalculator.formattedCurrentTaxAllowance" should {
+    "return a formatted string of the personal tax rate limit" when {
+      "the income falls into the personal tax band bracket" in {
+        val result: String = taxCalculator.formattedCurrentTaxAllowance(9000)
+
+        assert(result == "£10,000")
+      }
+    }
+  }
+
+  "TaxCalculator.formattedCurrentTaxAllowance" should {
+    "return string 'no limit'" when {
+      "the income is above higher income tax bracket" in {
+        val result: String = taxCalculator.formattedCurrentTaxAllowance(125001)
+
+        assert(result == "No limit")
+      }
+    }
+  }
+
+  /** capital gains tax from shares */
+
+  "TaxCalculator.calculateCapitalGainsFromShares" should {
+    "return string of the amount of tax to be paid from shares" when {
+      "the tax band is basic and shares are above taxable threshold" in {
+        val result: String = taxCalculator.calculateCapitalGainsFromShares(20000, 5000)
+
+        assert(result == "£500")
+      }
+    }
+  }
+
+  "TaxCalculator.calculateCapitalGainsFromShares" should {
+    "return string of the amount of tax to be paid from shares" when {
+      "the tax band is higher and shares are above taxable threshold" in {
+        val result: String = taxCalculator.calculateCapitalGainsFromShares(80000, 5000)
+
+        assert(result == "£1,000")
+      }
+    }
+  }
+
+  "TaxCalculator.calculateCapitalGainsFromShares" should {
+    "return string of the amount of tax to be paid from shares" when {
+      "the tax band is personal and shares are above taxable threshold" in {
+        val result: String = taxCalculator.calculateCapitalGainsFromShares(9000, 5000)
+
+        assert(result == "£500")
+      }
+    }
+  }
+
+  "TaxCalculator.calculateCapitalGainsFromShares" should {
+    "return 'below threshold'" when {
+      "the shares are worth below the taxable threshold with higher tax band" in {
+        val result: String = taxCalculator.calculateCapitalGainsFromShares(80000, 2999)
+
+        assert(result == "shares are below taxable threshold")
+      }
+    }
+  }
+
+  "TaxCalculator.calculateCapitalGainsFromShares" should {
+    "return 'below threshold'" when {
+      "the shares are worth below the taxable threshold with basic tax band" in {
+        val result: String = taxCalculator.calculateCapitalGainsFromShares(20000, 2999)
+
+        assert(result == "shares are below taxable threshold")
+      }
+    }
+  }
+
+  "TaxCalculator.calculateCapitalGainsFromShares" should {
+    "return 'below threshold'" when {
+      "the shares are worth below the taxable threshold with personal tax band" in {
+        val result: String = taxCalculator.calculateCapitalGainsFromShares(9000, 2999)
+
+        assert(result == "shares are below taxable threshold")
+      }
+    }
+  }
+
 }
